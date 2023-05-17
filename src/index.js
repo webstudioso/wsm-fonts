70bf75eab26011226b75c2d004ca5c9822baf14e
Giancarlo Sanchez <gianksp@gmail.com>
2023-05-17 19:49:33 +0200

    Adding Open Sans

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 export const WSMFontStyles = [
     'https://fonts.cdnfonts.com/css/strasua',
     'https://fonts.cdnfonts.com/css/good-times-2',
-    'https://fonts.cdnfonts.com/css/pirulen'
+    'https://fonts.cdnfonts.com/css/pirulen',
+    'https://fonts.cdnfonts.com/css/open-sans'
 ]
 
 export const addFonts = (editor) => {
     const styleManager = editor.StyleManager;
     const fontProperty = styleManager.getProperty('typography', 'font-family');
     fontProperty.set('options', [
+        {id: "'Open Sans', sans-serif", label: 'Open Sans'},
         ...fontProperty.get('options'),
         {id: "'Strasua', sans-serif", label: 'Strasua'},
         {id: "'Good Times Rg', sans-serif", label: 'Good Times'},
