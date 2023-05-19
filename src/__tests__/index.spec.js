3be4b6769a99418a444bc2b49bfded290b5ce374
Giancarlo Sanchez <gianksp@gmail.com>
2023-05-19 08:28:02 +0200

    More fonts

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -13,18 +13,21 @@ describe("Fonts plugin", () => {
             }
             Plugin(editor)
             expect(editor.on).toHaveBeenCalledWith('load', expect.any(Function))
-            expect(WSMFontStyles.length).toBe(3)
+            expect(WSMFontStyles.length).toBe(6)
         })
     })
 
     describe("addFonts", () => {
         it("Adds custom fonts to style manager", () => {
             const expected = [
+              {"id": "'Open Sans', sans-serif", "label": "Open Sans"},
+              {"id": "'Montserrat', sans-serif", "label": "Montserrat"},
               {"id": "a", "label": "A"}, 
               {"id": "b", "label": "B"}, 
               {"id": "'Strasua', sans-serif", "label": "Strasua"}, 
               {"id": "'Good Times Rg', sans-serif", "label": "Good Times"}, 
-              {"id": "'Pirulen Rg', sans-serif", "label": "Pirulen"}
+              {"id": "'Pirulen Rg', sans-serif", "label": "Pirulen"},
+              {"id": "'Abril Fatface', sans-serif", "label": "Abril Fatface"},
             ]
             const set = jest.fn()
             const editor = {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ export const WSMFontStyles = [
     'https://fonts.cdnfonts.com/css/strasua',
     'https://fonts.cdnfonts.com/css/good-times-2',
     'https://fonts.cdnfonts.com/css/pirulen',
-    'https://fonts.cdnfonts.com/css/open-sans'
+    'https://fonts.cdnfonts.com/css/open-sans',
+    'https://fonts.cdnfonts.com/css/montserrat',
+    'https://fonts.cdnfonts.com/css/abril-fatface'
 ]
 
 export const addFonts = (editor) => {
@@ -10,10 +12,12 @@ export const addFonts = (editor) => {
     const fontProperty = styleManager.getProperty('typography', 'font-family');
     fontProperty.set('options', [
         {id: "'Open Sans', sans-serif", label: 'Open Sans'},
+        {id: "'Montserrat', sans-serif", label: 'Montserrat'},
         ...fontProperty.get('options'),
         {id: "'Strasua', sans-serif", label: 'Strasua'},
         {id: "'Good Times Rg', sans-serif", label: 'Good Times'},
         {id: "'Pirulen Rg', sans-serif", label: 'Pirulen'},
+        {id: "'Abril Fatface', sans-serif", label: 'Abril Fatface'},
     ])
 }
 
